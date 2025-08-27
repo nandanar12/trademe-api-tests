5be7401b94467b150fc4f3b8e131066b64dade75
Ram Ganesan <mg.ramachandran1@gmail.com>
2025-08-27 12:19:38 +1200

    Updated

--- src/main/java/com/trademe/api/clients/ListingACarApi.java
+++ src/main/java/com/trademe/api/clients/ListingACarApi.java
@@ -5,6 +5,7 @@ import com.trademe.framework.config.ConfigPropertiesLoader;
 import com.trademe.framework.testbase.RequestBuilder;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 public class ListingACarApi {
 
